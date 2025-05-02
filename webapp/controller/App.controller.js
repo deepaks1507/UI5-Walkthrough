28b97375b7576aefeb645d0da9738d06028c6517
deepaks1507 <deepaksinghds2003@gmail.com>
2025-05-02 23:07:47 +0530

    Using JSON Model in View

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,10 +1,24 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], function(Controller){
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast",
+    "sap/ui/model/json/JSONModel"
+], function (Controller, MessageToast, JSONModel) {
     "use strict";
-    return Controller.extend("sap.ui.demo.walkthrough.controller.App",{
-        onSayHi: function(){
-            alert("Hello UI5");
+    return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
+
+        onInit: function () {
+            var OData = {
+                recepient: {
+                    name: "UI5"
+                }
+            };
+            var OModel = new JSONModel(OData);
+            this.getView().setModel(OModel);
+        },
+
+        onSayHi: function () {
+            // alert("Hello UI5");
+            MessageToast.show("Hello UI5");
         }
     })
 })
