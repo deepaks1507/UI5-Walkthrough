7f07f38598ec7ec8f2b757eb3c899e39b7cc75ec
deepaks1507 <deepaksinghds2003@gmail.com>
2025-05-02 23:35:57 +0530

    Used i18n text

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,12 +1,14 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/m/MessageToast",
-    "sap/ui/model/json/JSONModel"
-], function (Controller, MessageToast, JSONModel) {
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/resource/ResourceModel"
+], function (Controller, MessageToast, JSONModel, ResourceModel) {
     "use strict";
     return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
 
         onInit: function () {
+            //set the data model on the view
             var OData = {
                 recepient: {
                     name: "UI5"
@@ -14,11 +16,23 @@ sap.ui.define([
             };
             var OModel = new JSONModel(OData);
             this.getView().setModel(OModel);
+
+            //Set 18n model on view
+            var i18nModel = new ResourceModel({
+                bundleName: "sap.ui.demo.walkthrough.i18n.i18n",
+                supportedLocales: [""],
+                fallbackLocale: ""
+            });
+            this.getView().setModel(i18nModel, "i18n");
         },
 
         onSayHi: function () {
+            var oBundle = this.getView().getModel("i18n").getResourceBundle();
+            var sRecepient = this.getView().getModel().getProperty("/recepient/name");
+            var sMsg = oBundle.getText("helloMsg", [sRecepient]);
             // alert("Hello UI5");
-            MessageToast.show("Hello UI5");
+            // MessageToast.show("Hello UI5");
+            MessageToast.show(sMsg);
         }
     })
 })
