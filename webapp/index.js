220e6cd79ff89046afc8fed8dda271ffae12a609
deepaks1507 <deepaksinghds2003@gmail.com>
2025-05-02 21:41:20 +0530

    Using Views

--- webapp/index.js
+++ webapp/index.js
@@ -1,10 +1,17 @@
 sap.ui.define([
-    "sap/m/Text"
+    "sap/ui/core/mvc/XMLView"
 
-], function(Text) {
+], function(XMLView) {
     "use strict";
-    alert("Hello UI5");
-    new Text({
-        text: "Hello UI5"
-    }).placeAt("content");
+    // alert("Hello UI5");
+
+    // new Text({
+    //     text: "Hello UI5"
+    // }).placeAt("content");
+
+    XMLView.create({
+        viewName:"sap.ui.walkthrough.view.App"
+    }).then(function(oView){
+        oView.placeAt("content");
+    })
 });
