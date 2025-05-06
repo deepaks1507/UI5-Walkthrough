0ff16cee7e7595e8c7c47c2fe352485bcb12c355
deepaks1507 <deepaksinghds2003@gmail.com>
2025-05-06 17:04:00 +0530

    Fragment Callbacks

--- webapp/controller/HelloPannel.controller.js
+++ webapp/controller/HelloPannel.controller.js
@@ -21,6 +21,7 @@ sap.ui.define([
                 Fragment.load({
                     id: oView.getId(),
                     name: "sap.ui.demo.walkthrough.view.HelloDialog",
+                    controller: this
                 }).then(function(oDialog){
                     // connect dialog to the root view of this component (models, lifecycle)
                     oView.addDependent(oDialog);
@@ -31,6 +32,14 @@ sap.ui.define([
                 this.byId("helloDialog").open();
             }
         },
+
+        onCloseDialog: function (){
+            // var oDialog = this.byId("helloDialog");
+            // oDialog.close();
+            console.log("Dialog closed");
+
+            this.byId("helloDialog").close();
+        }
 
     })
 });
